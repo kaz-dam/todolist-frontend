c9db990f27e0ca276835724981f594cad28f9b3b
Adam <kazai.adam@gmail.com>
2023-10-01 19:44:49 +0200

    Add the route components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
 import Footer from "./components/footer";
@@ -6,15 +11,22 @@ import "./App.css";
 
 const App = () => {
     return (
-        <div className="flex md:flex-row flex-col w-screen h-screen bg-todo-green text-todo-text">
-            <Sidebar />
-            <div className="w-full md:h-screen flex flex-col flex-grow bg-todo-light-grey rounded-tl-todo md:rounded-bl-todo rounded-bl-none md:rounded-tr-none rounded-tr-todo py-5 md:px-12 px-4 relative">
-                <Header siteTitle="Dashboard" />
-                {/* ROUTER GOES HERE */}
-                <div className="grow">content</div>
-                <Footer />
+        <Router>
+            <div className="flex md:flex-row flex-col w-screen h-screen bg-todo-green text-todo-text">
+                <Sidebar />
+                <div className="w-full md:h-screen flex flex-col flex-grow bg-todo-light-grey rounded-tl-todo md:rounded-bl-todo rounded-bl-none md:rounded-tr-none rounded-tr-todo py-5 md:px-12 px-4 relative">
+                    <Header siteTitle="Dashboard" />
+                    <div className="grow">
+                        <Routes>
+                            <Route path="/" element={<h1>This is the dashboard</h1>} />
+                            <Route path="statistics" element={<h1>This is the statistics</h1>} />
+                            <Route path="calendar" element={<h1>This is the calendar</h1>} />
+                        </Routes>
+                    </div>
+                    <Footer />
+                </div>
             </div>
-        </div>
+        </Router>
     );
 };
 
