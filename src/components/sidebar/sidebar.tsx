188ebf68209cc350f2a2f3231f52e60447ba8b81
Adam <kazai.adam@gmail.com>
2023-10-01 20:16:33 +0200

    Add links to the sidebar menu

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Icon from "../ui/icon";
 import SideBarMenu from "../ui/sidebar-menu/sidebar-menu";
 
 const Sidebar = () => {
     return (
         <div className="md:w-[110px] w-full md:h-auto bg-todo-green text-white md:py-10 py-4 md:px-0 px-4 flex md:flex-col flex-row items-center justify-between">
-            <h3 className="">YuDo</h3>
+            <Link to="/"><h3 className="">YuDo</h3></Link>
             <SideBarMenu>
-                <li><Icon iconName="dashboard" /></li>
-                <li><Icon iconName="leaderboard" /></li>
-                <li><Icon iconName="calendar_month" /></li>
+                <li><Link to="/"><Icon iconName="dashboard" /></Link></li>
+                <li><Link to="/statistics"><Icon iconName="leaderboard" /></Link></li>
+                <li><Link to="/calendar"><Icon iconName="calendar_month" /></Link></li>
             </SideBarMenu>
             <div><Icon iconName="logout" /></div>
         </div>
