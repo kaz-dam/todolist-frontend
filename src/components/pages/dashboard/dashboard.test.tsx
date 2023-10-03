3c7ed739646282bc54e20556ca3d49c29ed62aee
Adam <kazai.adam@gmail.com>
2023-10-03 18:33:17 +0200

    Fix dashboard test issue

--- src/components/pages/dashboard/dashboard.test.tsx
+++ src/components/pages/dashboard/dashboard.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render } from "../../../lib/test-utils";
 import Dashboard from "./dashboard";
 
 test("renders without crashing", () => {
