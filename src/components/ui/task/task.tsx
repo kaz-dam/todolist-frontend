c47c5a055b2f8a8c0ddddae9f63ee16fac947fb2
Adam <kazai.adam@gmail.com>
2023-10-03 21:44:40 +0200

    Add mark completed functionality

--- src/components/ui/task/task.tsx
+++ src/components/ui/task/task.tsx
@@ -11,7 +11,7 @@ const Task = ({ taskTitle, deadLine, onMarkCompleted, ...props }: TaskProps) => 
     const [ isCompleted, setIsCompleted ] = useState(false);
 
     const markCompleted = () => {
-        setIsCompleted(true);
+        setIsCompleted(!isCompleted);
         onMarkCompleted();
     };
 
@@ -19,7 +19,9 @@ const Task = ({ taskTitle, deadLine, onMarkCompleted, ...props }: TaskProps) => 
         <div className="flex flex-row justify-between items-center w-full py-4 px-10 bg-white">
             <div className="flex flex-row justify-between items-center gap-8">
                 <Icon iconName="drag_indicator" className="text-todo-green cursor-grab" />
-                <span onClick={markCompleted} className={`block border-2 border-todo-green w-6 h-6 `}></span>
+                <span onClick={markCompleted} className={`flex justify-center items-center cursor-pointer border-2 border-todo-green w-6 h-6 ${isCompleted && 'bg-todo-green text-white'}`}>
+                    {isCompleted && <Icon iconName="check" className="text-white" />}
+                </span>
                 <h2 className="font-semibold text-lg">{taskTitle}</h2>
             </div>
             <span>{deadLine}</span>
