525989e3b3282fd84d09b17b68450a37985d341f
Adam <kazai.adam@gmail.com>
2023-10-18 12:55:27 +0200

    Fix floating button test

--- src/components/ui/floating-button/floating-button.test.tsx
+++ src/components/ui/floating-button/floating-button.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render } from "../../../lib/test-utils";
 import FloatingButton from "./floating-button";
 
 test("renders without crashing", () => {
